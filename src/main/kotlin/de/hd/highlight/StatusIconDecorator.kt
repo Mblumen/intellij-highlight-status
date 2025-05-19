bf18b680f95c0440e29d18f7ce8e34b68807595b
Max Blumenstock <max.blumenstock@med.uni-heidelberg.de>
2025-05-19 10:30:26 +0200

    Enhance status decorator functionality and update configurations
    Refined `StatusIconDecorator` to support additional statuses, dynamic coloring, and conditional icons. Updated `vcs.xml` and adjusted Gradle configurations, including disabling the Java home path. Minor IntelliJ self-update lock timestamp change.

--- src/main/kotlin/de/hd/highlight/StatusIconDecorator.kt
+++ src/main/kotlin/de/hd/highlight/StatusIconDecorator.kt
@@ -1,15 +1,17 @@
 package de.hd.highlight
 
-import com.intellij.ide.projectView.ProjectViewNodeDecorator
+import com.intellij.icons.AllIcons
 import com.intellij.ide.projectView.PresentationData
+import com.intellij.ide.projectView.ProjectViewNode
+import com.intellij.ide.projectView.ProjectViewNodeDecorator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.openapi.vfs.VfsUtilCore
 import com.intellij.psi.PsiManager
-import com.intellij.util.PlatformIcons
-import javax.swing.Icon
-import com.intellij.ide.projectView.ProjectViewNode
+import com.intellij.ui.JBColor
 import com.intellij.ui.SimpleTextAttributes
+import java.awt.Color
+import javax.swing.Icon
+
 
 class StatusIconDecorator : ProjectViewNodeDecorator {
     override fun decorate(node: ProjectViewNode<*>, data: PresentationData) {
@@ -20,20 +22,46 @@ class StatusIconDecorator : ProjectViewNodeDecorator {
         val text = psiFile.text
 
         if (text.contains("// STATUS: DONE")) {
-            data.setIcon(DoneIcon)
+            if(text.contains("// ICON")) {
+                data.setIcon(DoneIcon)
+            }
             val originalPresentation = data.presentableText
             data.clearText()
             data.addText(originalPresentation, SimpleTextAttributes.REGULAR_ATTRIBUTES)
-            data.addText(" [DONE]", SimpleTextAttributes.GRAY_ATTRIBUTES)
+            data.addText(" [DONE]",
+                SimpleTextAttributes(SimpleTextAttributes.STYLE_BOLD, JBColor(Color(0x389e34), Color(0x499c54))))
         } else if (text.contains("// STATUS: WIP")) {
-            data.setIcon(WipIcon)
+            if(text.contains("// ICON")) {
+                data.setIcon(WipIcon)
+            }
             val originalPresentation = data.presentableText
             data.clearText()
             data.addText(originalPresentation, SimpleTextAttributes.REGULAR_ATTRIBUTES)
-            data.addText(" [IN PROGRESS]", SimpleTextAttributes.SYNTHETIC_ATTRIBUTES)
+            data.addText(" [IN PROGRESS]",
+                SimpleTextAttributes(SimpleTextAttributes.STYLE_BOLD, JBColor(Color(0xd98a00), Color(0xe0a431))))
+        } else if (text.contains("// STATUS: TODO")) {
+            if(text.contains("// ICON")) {
+                data.setIcon(TodoIcon)
+            }
+            val originalPresentation = data.presentableText
+            data.clearText()
+            data.addText(originalPresentation, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+            data.addText(" [TODO]",
+                SimpleTextAttributes(SimpleTextAttributes.STYLE_PLAIN, JBColor(Color(0x3d5afe), Color(0x539dfc))))
+        } else if (text.contains("// STATUS: INACTIVE")) {
+            if(text.contains("// ICON")) {
+                data.setIcon(InactiveIcon)
+            }
+            val originalPresentation = data.presentableText
+            data.clearText()
+            data.addText(originalPresentation, SimpleTextAttributes.REGULAR_ATTRIBUTES)
+            data.addText(" [INACTIVE]", SimpleTextAttributes.GRAYED_ATTRIBUTES)
         }
+
     }
 
-    object DoneIcon : Icon by PlatformIcons.CHECK_ICON
-    object WipIcon : Icon by PlatformIcons.CHECK_ICON_SELECTED
+    object DoneIcon : Icon by AllIcons.General.GreenCheckmark
+    object WipIcon : Icon by AllIcons.Process.Step_1
+    object TodoIcon : Icon by AllIcons.General.TodoDefault
+    object InactiveIcon : Icon by AllIcons.General.Error
 }
